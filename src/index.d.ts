5e68f5c0fec6d8e1d6c9e15f10891af9d4a4fde9
mishamyrt <misha@myrt.co>
2021-08-12 00:22:01 +0400

    Add typings

---
+++ src/index.d.ts
@@ -0,0 +1,6 @@
+/**
+ * Returns all element descendants of node that match selectors.
+ * @param q - query
+ * @param c - context
+ */
+export function $(q: string, c?: HTMLElement): HTMLElement | HTMLElement[] | undefined
---
+++ src/index.js
@@ -0,0 +1,1 @@
+export const $ = (b=>(q,c=document)=>(b=[...c.querySelectorAll(q)],b.length>1?b:b[0]))()
